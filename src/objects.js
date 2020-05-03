8c279a67751389b2c6afefe89009e00ef89c8b0b
Viktoriia Petrova <etrovavica1221@gmail.com>
2020-05-03 18:14:38 +0100

    Add finished test for objects

--- src/objects.js
+++ src/objects.js
@@ -1,47 +1,65 @@
 const createPerson = (name, age) => {
-  // your code here
+  const createPerson = {
+    name: name,
+    age: age
+  };
+
+  return createPerson;
+
 };
 
 const getName = object => {
-  // your code here
+  return object.name;
 };
 
 const getProperty = (property, object) => {
-  // your code here
+  return object[property];
 };
 
 const hasProperty = (property, object) => {
-  // your code here
+  return object.hasOwnProperty(property);
 };
 
 const isOver65 = person => {
-  // your code here
+  if (person.age > 65) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 const getAges = people => {
-  // your code here
+  return people.map(person => person.age);
 };
 
 const findByName = (name, people) => {
-  // your code here
+  return people.find(person => person.name === name);
 };
 
 const findHondas = cars => {
-  // your code here
+  return cars.filter(car => car.manufacturer === "Honda");
 };
 
 const averageAge = people => {
-  // your code here
+  return people.reduce(function(a,person) {
+    return a + person.age;
+  },0)/people.length;
 };
 
 const createTalkingPerson = (name, age) => {
-  // your code here
+  return {
+    name: name,
+    age: age,
+    introduce(friendName) {
+      return `Hi ${friendName}, my name is ${this.name} and I am ${this.age}!`
+    }
+  };
 };
 
 module.exports = {
   createPerson,
   getName,
-  getProperty,
+  getProperty, 
   hasProperty,
   isOver65,
   getAges,
